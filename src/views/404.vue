7cf5f3be89eec0f0956d44303e301d67e75028fe
guilhermerodrigues680 <guilhermerodrigues680@gmail.com>
2020-12-23 03:23:25 -0300

    Setup Vuetify Base Wireframe

---
+++ src/views/404.vue
@@ -0,0 +1,44 @@
+<template>
+  <v-container fluid class="fill-height align-start">
+    <v-container fluid class="ma-0 pa-0">
+
+    <v-row>
+      <v-col>
+        <h1 class="text-h3 font-weight-light">
+          Página não encontrada
+        </h1>
+      </v-col>
+    </v-row>
+
+    <v-row>
+      <v-col>
+        <p class="text-h5">A página que você procura não existe.</p>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col>
+        <v-img
+          :src="require('../assets/not-found.png')"
+          contain
+          height="200"
+        />
+      </v-col>
+    </v-row>
+    </v-container>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: '404',
+  mounted () {
+    console.log(this, '404 - Página não encontada', this.$router.currentRoute.path)
+  }
+}
+</script>
+
+<style scoped>
+  span {
+    background-color: pink;
+  }
+</style>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,20 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <v-container fluid class="fill-height align-start">
+    <v-container fluid class="ma-0 pa-0">
+
+    <v-row>
+      <v-col>
+        <h1 class="text-h3 font-weight-light">
+          About
+        </h1>
+      </v-col>
+    </v-row>
+
+    <v-row>
+      <v-col>
+        <p class="text-h5">This is an about page</p>
+      </v-col>
+    </v-row>
+    </v-container>
+  </v-container>
 </template>
